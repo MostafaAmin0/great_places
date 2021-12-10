3a6625ffa20dd09d76bf716c532294684b05f341
MostafaAmin0 <mostafa.aminmk@gmail.com>
2021-12-10 19:19:19 +0200

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../screens/places_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.indigo)
+            .copyWith(secondary: Colors.amber),
+      ),
+      home: const PlacesListScreen(),
+    );
+  }
+}
