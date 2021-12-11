7fde143ad525c2268ff42b2327e3a69adb817224
MostafaAmin0 <mostafa.aminmk@gmail.com>
2021-12-11 23:07:05 +0200

    fix camera bug

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -21,13 +21,14 @@ class _ImageInputState extends State<ImageInput> {
       source: source,
       maxWidth: 600,
     );
+    if (imageFile == null) {
+      return;
+    }
     setState(() {
-      if (imageFile != null) {
-        _storedImage = File(imageFile.path);
-      }
+      _storedImage = File(imageFile.path);
     });
     final appDir = await syspaths.getApplicationDocumentsDirectory();
-    final fileName = path.basename(imageFile!.path);
+    final fileName = path.basename(imageFile.path);
     final savedImage = await _storedImage!.copy('${appDir.path}/$fileName');
   }
 
