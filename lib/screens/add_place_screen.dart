eafb8e80c4af56c921f9cb7c66189de178293235
MostafaAmin0 <mostafa.aminmk@gmail.com>
2021-12-12 16:07:51 +0200

    pass image tp parent widget

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:great_places/providers/places_provider.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/image_input.dart';
 
@@ -13,6 +17,20 @@ class AddPlaceScreen extends StatefulWidget {
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+    Provider.of<PlacesProvider>(context, listen: false)
+        .addPlace(_titleController.text, _pickedImage as File);
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +54,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     const SizedBox(
                       height: 10,
                     ),
-                    const ImageInput(),
+                    ImageInput(_selectImage),
                   ],
                 ),
               ),
@@ -45,7 +63,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
           ElevatedButton.icon(
             icon: const Icon(Icons.add),
             label: const Text('Add Place'),
-            onPressed: () {},
+            onPressed: _savePlace,
             style: ElevatedButton.styleFrom(
               elevation: 0,
               tapTargetSize: MaterialTapTargetSize.shrinkWrap,
