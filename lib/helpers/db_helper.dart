f4de5c4e7e2c518ae85075e0b09beb76bd4e8713
MostafaAmin0 <mostafa.aminmk@gmail.com>
2021-12-12 14:43:04 +0200

    sqflite package & create database

---
+++ lib/helpers/db_helper.dart
@@ -0,0 +1,28 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqlite_api.dart';
+
+class DBHelper {
+  static Future<Database> database() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(
+      path.join(dbPath, 'places.db'),
+      /// for the first time it there is not database
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
+      },
+      version: 1,
+    );
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    final db = await DBHelper.database();
+    db.insert(
+      table,
+      data,
+      conflictAlgorithm: ConflictAlgorithm.replace,///overrride if exists
+    );
+  }
+
+}
